af79a4e7ed3b8fc2a8092cfe18069f475d4b6186
Brayan Llanos <brayan0428@gmail.com>
2020-04-16 17:52:53 -0500

    Configuracion inicial con Webpack

---
+++ src/containers/App.svelte
@@ -0,0 +1,16 @@
+<style>
+  :global(body) {
+    background-color: #676778;
+    color: white;
+  }
+</style>
+
+<div class="main">
+  <h1>Hello Pugstagram</h1>
+  <p>
+    Lorem ipsum, dolor sit amet consectetur adipisicing elit. Harum accusantium,
+    ut dolor magnam quidem eligendi iusto totam perferendis perspiciatis.
+    Assumenda doloremque rem illum nisi quibusdam voluptate amet odit pariatur
+    incidunt.
+  </p>
+</div>
