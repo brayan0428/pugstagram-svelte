5bfd3303cde3759718494d600ee13214e1d1bb0b
Brayan Llanos <brayan0428@gmail.com>
2020-04-16 21:07:39 -0500

    Se agregaron los estilos de la aplicación

--- src/components/Sidebar.svelte
+++ src/components/Sidebar.svelte
@@ -1,8 +1,18 @@
 <script>
-  import Profile from "./components/Profile.svelte";
-  import Stories from "./components/Stories.svelte";
-  import Footer from "./components/Footer.svelte";
+  import Profile from "./Profile.svelte";
+  import Stories from "./Stories.svelte";
+  import Footer from "./Footer.svelte";
 </script>
+
+<style>
+  .Sidebar {
+    position: relative;
+    padding: 4.5em 0 0 0;
+  }
+  .Sidebar-container {
+    position: fixed;
+  }
+</style>
 
 <div class="Sidebar">
   <div class="Sidebar-container">
--- src/components/Timeline.svelte
+++ src/components/Timeline.svelte
@@ -1,1 +1,15 @@
-<div class="Timeline">Timeline</div>
+<script>
+  import Card from "./Card.svelte";
+</script>
+
+<style>
+  .Timeline {
+    padding: 4em 0 0 0;
+  }
+</style>
+
+<div class="Timeline">
+  <div class="Timeline-container">
+    <Card />
+  </div>
+</div>
